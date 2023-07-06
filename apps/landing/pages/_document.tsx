002d82a1b34f20363017004145112a72e0a741ae
kaylendog <actuallyori@gmail.com>
2023-07-06 20:39:54 +0100

    feat: synchronise landing repo

---
+++ apps/landing/pages/_document.tsx
@@ -0,0 +1,75 @@
+import Document, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<meta charSet="UTF-8" />
+
+					<meta
+						name="description"
+						content="Commission platform and artist network."
+					/>
+					<meta
+						name="keywords"
+						content="art, commission, platform, social media"
+					/>
+					<meta name="author" content="kaylen" />
+
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+					/>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="*"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
