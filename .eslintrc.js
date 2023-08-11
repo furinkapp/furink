5e0fe6d2f680637125a5260d615efa7c49486997
Kaylen Dart <actuallyori@gmail.com>
2023-08-11 17:31:51 +0100

    chore: scaffold basic nestjs app

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,12 @@ module.exports = {
 	extends: "eslint-config-infernal",
 	parserOptions: {
 		tsconfigRootDir: __dirname,
-		project: ["./packages/*/tsconfig.json", "apps/*/tsconfig.json"],
+		project: ["./packages/*/tsconfig*.json", "apps/*/tsconfig*.json", "services/*/tsconfig*.json"],
 	},
 	root: true,
+	rules: {
+		// broken in eslint-config-infernal
+		"new-cap": "off",
+	},
 	ignorePatterns: ["next-env.d.ts"],
 };
